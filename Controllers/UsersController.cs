4251c2290be7945aedca4fc4d7248f38cfc7286e
aybukeberfinsuer <aybukeberfinsuer@gmail.com>
2024-12-29 23:39:22 +0300

    login process

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using ProductsAPI.DTO;
+using ProductsAPI.Models;
+
+namespace ProductsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController:ControllerBase{
+        private UserManager<AppUser> _userManager;
+        private readonly SignInManager<AppUser> _signInManager;
+
+        public UsersController(UserManager<AppUser> userManager,SignInManager<AppUser> signInManager){
+            _userManager=userManager;
+            _signInManager=signInManager;
+        } 
+
+        [HttpPost("register")]
+
+        public async Task<IActionResult> CreateUser (UserDto model){
+            if(!ModelState.IsValid){
+                return BadRequest(ModelState);
+            }
+
+            var user= new AppUser{
+                FullName= model.FullName,
+                UserName=model.UserName,
+                Email=model.Email,
+                DateAdded= DateTime.Now
+            };
+
+            var result= await _userManager.CreateAsync(user,model.Password);
+            if(result.Succeeded){
+                return StatusCode(201);
+            }
+            return BadRequest(result.Errors);
+            
+           
+
+        }
+
+
+        public async Task<IActionResult> Login(LoginDto model){
+
+            var user= await _userManager.FindByEmailAsync(model.Email);
+            if(user==null){
+                return BadRequest( new{message="incorrect username"});
+
+            }
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user,model.Password,false);
+
+            if(result.Succeeded){
+                return Ok(new{ token="token"});
+            }
+
+            return Unauthorized();
+
+
+
+    }
+
+
+
+
+    }
+    
+
+    
+}
---
+++ DTO/LoginDto.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ProductsAPI.DTO
+{
+    public class LoginDto
+    {
+        [Required]
+        public string? Email { get; set; }
+
+        [Required]
+        public string? Password { get; set; }
+        
+    }
+   
+    
+}
